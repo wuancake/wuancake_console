8c24928d0016fd63ebd6b9fc32467c25f2219970
Gtacer <459472218@qq.com>
2017-06-04 17:34:12 +0800

    css和js

---
+++ public/static/admin/js/station.js
@@ -0,0 +1,18 @@
+//创建导师
+function create_tutor() {
+    document.getElementById('tutor').style.display = 'block';
+}
+
+//创建管理员
+function create_admin() {
+    document.getElementById('admin').style.display = 'block';
+}
+//隐藏创建导师列表
+function hid_tutor() {
+    document.getElementById('tutor').style.display = 'none';
+}
+
+//隐藏创建管理员列表
+function hid_admin() {
+    document.getElementById('admin').style.display = 'none';
+}
