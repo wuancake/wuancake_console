6766c841437a4f03aebfa7716d99daf1408d4195
ichenchao <chenchao.i@qq.com>
2017-06-12 23:18:20 +0800

    修正样式和按最近周排序

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -18,7 +18,7 @@ class Index extends Controller
             $this->error('您没有权限操作，请联系管理员','login/log');
         }
         $userres = \think\Db::name('user') -> field('id,user_name') -> select();
-        $weekres = \think\Db::name('report') -> distinct('ture') -> field('week_num') -> select();
+        $weekres = \think\Db::name('report') -> distinct('ture') -> field('week_num') -> order('week_num desc') -> select();
         $this->assign('userres',$userres);
         $this->assign('weekres',$weekres);
         return $this->fetch();
@@ -43,7 +43,7 @@ class Index extends Controller
 
         }
         $userres = \think\Db::name('user')->where('id',$data['user_id'])->find();
-        $reportres= \think\Db::name('report')->where('user_id',$data['user_id'])->paginate(10 , false , ['query' => request() -> param(),]);
+        $reportres= \think\Db::name('report')->where('user_id',$data['user_id'])->order('week_num desc')->paginate(10 , false , ['query' => request() -> param(),]);
         $data['user_name']= $userres['user_name'];
         $this->assign('data',$data);
         $this->assign('reportres',$reportres);
