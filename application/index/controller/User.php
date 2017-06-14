cb4b68e8b6127fd5ac400aaf08f42c12ef6ee61b
Gtacer <459472218@qq.com>
2017-06-14 11:13:13 +0800

    增加对用户表单数据的后台验证

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -4,7 +4,6 @@ namespace app\index\controller;
 
 use app\index\model\Attend;
 use app\index\model\User AS UserModel;
-use app\index\model\UserGroup;
 use think\Controller;
 use think\Db;
 use think\Request;
@@ -13,15 +12,7 @@ use think\Session;
 
 class User extends Controller
 {
-//    public function test(){
-//        return view();
-//    }
-
     //登录界面
-    public function test()
-    {
-        return view('log');
-    }
     public function log(){
         if (session('token'))
             $this->error('你已登录！','user/login');
@@ -34,37 +25,56 @@ class User extends Controller
             $this->error('非法访问！请先登录','user/log');
         return view();
     }
-//    public function check_cap()
-//    {
-//        $captcha = new Captcha();
-//        $code = Request::instance()->param('code');
-//        if (!$captcha->check($code)){
-//            echo '验证码不正确';
-//        }else{
-//            echo '验证码正确!';
-//        }
-//    }
 
     //注册
     public function add(){
         $UserModel = new UserModel();
-//        $captcha = new Captcha();
         $Attend = new Attend();
-//        $Group = new UserGroup();
-
-//        //判断验证码是否正确
-//        $code = Request::instance()->param('code');
-//        if (!$captcha->check($code))
-//            $this->error('验证码错误！');
 
         //获取数据
         $username = Request::instance()->param('user_name');
         $email = Request::instance()->param('email');
+        $qq = Request::instance()->param('QQ');
+        $wuan_name = Request::instance()->param('wuan_name');
+        $psd = Request::instance()->param('password');
         $result1 = $UserModel->where('user_name',"$username")->find();
         $result2 = $UserModel->where('email',"$email")->find();
 
+        //判断用户名是否为空
+        if ($username == ''){
+            $this->error('用户民不能为空！');
+        }
+
+        //检测用户名中是否存在空格
+        if (strpos($username,' ') !== false){
+            $this->error('用户名中不能包含空格');
+        }
+
+        //检测邮箱是否合法
+        if ($email == '' || strpos($email,' ') !== false){
+            $this->error('邮箱帐号不能为空或存在空格');
+        }
+
+        //判断QQ号是否合法
+        if ($qq == '' || strpos($qq,' ') !== false){
+            $this->error('QQ号不能为空或存在空格');
+        }
+        elseif (!is_numeric($qq)){
+            $this->error('QQ号必须是纯数字字符串');
+        }
+
+        //判断午安昵称是否合法
+        if ($wuan_name == '' || strpos($wuan_name,' ') !== false){
+            $this->error('午安昵称不能为空或存在空格');
+        }
+
+        //判断密码是否合法
+        if ($psd == '' || strpos($psd,' ') !== false){
+            $this->error('密码不能为空或存在空格');
+        }
+
         //判断两次输入的密码是否一致
-        if (Request::instance()->param('password') != Request::instance()->param('repassword'))
+        if ($psd != Request::instance()->param('repassword'))
             $this->error('两次输入的密码不一致！请检查并重新输入！');
 
         //判断用户名是否存在
@@ -81,10 +91,6 @@ class User extends Controller
             $Attend->group_id = 0;
             $Attend->save();
 
-            //将用户分组信息写入分组表
-//            $Group->user_id = $UserModel->id;
-//            $Group->group_id = 0;
-//            $Group->save();
             //新注册用户无须在此时选择分组 201706091254 xiaochao
             $this->success('注册成功,即将转向登录界面','user/log');
         }
@@ -99,22 +105,26 @@ class User extends Controller
             $this->redirect('index/index');
         }
         $User = new UserModel();
- //       $Group = new UserGroup();
-//        $captcha = new Captcha();
 
-//        //判断验证码是否正确
-//        $code = Request::instance()->param('code');
-//        if (!$captcha->check($code))
-//            $this->error('验证码错误！','user/log');
+        $email = Request::instance()->param('email');
+        //判断邮箱是否为空或存在空格
+        if ($email == '' || strpos($email,' ') !== false){
+            $this->error('输入的邮箱格式有误，请检查后重新输入');
+        }
 
         //判断输入的邮箱是否已注册
-        $email = Request::instance()->param('email');
         $resemail = $User->where('email',"$email")->find();
         if (!isset($resemail))
             $this->error('该邮箱尚未注册网站');
 
         //判断密码是否正确
         $password = Request::instance()->param('password');
+
+        //判断密码是否格式正确
+        if ($password == '' || strpos($password,' ') !== false){
+            $this->error('密码不能为空或存在空格');
+        }
+
         $info = $User->where('email',"$email")->find();
         if ($info->password == md5($password)) {
             session('token',"$info->id");
@@ -143,11 +153,8 @@ class User extends Controller
             $this->error('非法参数！');
         //防止用户通过修改地址重新加入分组
         $UserModel = new UserModel();
-        $Group = new UserGroup();
         //判断用户是否加入分组 20170609 新增
         if ($UserModel->exist_user_group($id))
-        //if ($UserModel->where('id',session('token'))->find()->group_id)
-//        if ($Group ->where('user_id',session('token'))->find()->group_id)
             $this->error('你已加入分组！');
         //将数据更新到数据库
         //$res = $UserModel->where('id',"$id")->setField('group_id',$group);
