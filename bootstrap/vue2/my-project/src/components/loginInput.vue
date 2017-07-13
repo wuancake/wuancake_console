8cd0fb05eba9fe341184f3d859d352ea1af313f7
tim760255458 <qq760255458@gmail.com>
2017-07-13 13:53:34 +0800

    模版文件

---
+++ bootstrap/vue2/my-project/src/components/loginInput.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="loginInput">
+      <input v-model="msg" placeholder="输入电子邮箱" style="width: 300px"></input>
+      <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'login-input',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
