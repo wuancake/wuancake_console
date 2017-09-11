b5e27512a173b62399d0036d922fb998a75c381f
Gtacer <459472218@qq.com>
2017-09-11 18:57:11 +0800

    踢人接口文件

---
+++ api/admin/kick_someone.php
@@ -0,0 +1,35 @@
+<?php
+function json(array $info)
+{
+    echo json_encode($info);
+    die();
+}
+
+empty($_GET['session']) and json(['error' => '缺少必要的参数:session_id']);
+session_id($_GET['session']);
+session_start();
+empty($_SESSION['admin']) and json(['error' => '错误的session_id']);
+empty($_GET['user_id']) and json(['error' => '缺少必要的参数:user_id']);
+
+$connect = new mysqli('localhost', 'root', 'root', 'weekly');
+if ($connect->connect_error) {
+    json(['error' => '数据库连接出错']);
+}
+
+$user_id = $_GET['user_id'];
+$user_group = $connect->query("SELECT group_id FROM user_group WHERE user_id = $user_id")->fetch_assoc()['group_id'];
+
+$headsman = $_SESSION['admin']['name'];
+$_SESSION['admin']['auth'] === 1 && $_SESSION['admin']['group'] !== $user_group and json( ['error'=>'非法请求，导师只能踢出本组的人']);
+
+$time = date('Y-m-d H:m:s');
+
+$res = $connect->query("UPDATE user_group SET deleteFlg = 1 , headsman = $headsman ,modify_time = $time
+                                    WHERE user_id = $user_id AND create_time IN 
+                                    (SELECT value FROM 
+                                    (SELECT max(create_time) AS value FROM user_group WHERE user_id = $user_id ORDER BY create_time)
+                                    AS gp);");
+
+$res or json(['error'=>'踢人失败']);
+
+json(['success'=>'踢人成功']);
