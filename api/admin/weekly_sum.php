a2411c3457423754426ed78721255b51a71cd22c
Gtacer <459472218@qq.com>
2017-09-12 22:36:29 +0800

    更改周数为倒叙

--- api/admin/weekly_sum.php
+++ api/admin/weekly_sum.php
@@ -18,9 +18,9 @@ if ($connect->connect_error) {
 
 //从前一周开始，记录四周
 $week_num1 = ceil((time() - strtotime('2015-11-02')) / 604800)-1;
-$week_num2 = --$week_num1;
-$week_num3 = --$week_num2;
-$week_num4 = --$week_num3;
+$week_num2 = $week_num1-1;
+$week_num3 = $week_num2-1;
+$week_num4 = $week_num3-1;
 
 $data['data'] = array();
 
@@ -34,10 +34,10 @@ if (empty($_GET['week'])) {
         $res = $connect->query("
              SELECT user.id,a.group_id,user.user_name,user.qq,a.status AS week1,b.status AS week2,c.status AS week3,d.status AS week4
              FROM 
-             (((report AS a INNER JOIN user ON a.user_id = user.id AND a.week_num = $week_num4) 
-             INNER JOIN report AS b ON a.user_id = b.user_id AND b.week_num = $week_num3)
-             INNER JOIN report AS c ON a.user_id = c.user_id AND c.week_num = $week_num2)
-             INNER JOIN report AS d ON a.user_id = d.user_id AND d.week_num = $week_num1;");
+             (((report AS a INNER JOIN user ON a.user_id = user.id AND a.week_num = $week_num1) 
+             INNER JOIN report AS b ON a.user_id = b.user_id AND b.week_num = $week_num2)
+             INNER JOIN report AS c ON a.user_id = c.user_id AND c.week_num = $week_num3)
+             INNER JOIN report AS d ON a.user_id = d.user_id AND d.week_num = $week_num4;");
 
         while ($foo = $res->fetch_assoc()) { $data['data'][] = $foo;}
 
@@ -51,10 +51,10 @@ if (empty($_GET['week'])) {
 
         $res = $connect->query("SELECT user.id,a.group_id,user.user_name,user.qq,a.status AS week1,b.status AS week2,c.status AS week3,d.status AS week4
              FROM 
-             (((report AS a INNER JOIN user ON a.user_id = user.id AND a.week_num = $week_num4) 
-             INNER JOIN report AS b ON a.user_id = b.user_id AND b.week_num = $week_num3)
-             INNER JOIN report AS c ON a.user_id = c.user_id AND c.week_num = $week_num2)
-             INNER JOIN report AS d ON a.user_id = d.user_id AND d.week_num = $week_num1 WHERE a.group_id = {$_GET['group']};");
+             (((report AS a INNER JOIN user ON a.user_id = user.id AND a.week_num = $week_num1) 
+             INNER JOIN report AS b ON a.user_id = b.user_id AND b.week_num = $week_num2)
+             INNER JOIN report AS c ON a.user_id = c.user_id AND c.week_num = $week_num3)
+             INNER JOIN report AS d ON a.user_id = d.user_id AND d.week_num = $week_num4 WHERE a.group_id = {$_GET['group']};");
 
         while ($foo = $res->fetch_assoc()) { $data['data'][] = $foo; }
 
@@ -67,16 +67,16 @@ if (empty($_GET['week'])) {
 
     $week_num1 = $_GET['week'];
     $week_num1 > (ceil((time() - strtotime('2015-11-02')) / 604800)-1) and json(['error'=>'不能获取到本周或本周之后']);
-    $week_num2 = --$week_num1;
-    $week_num3 = --$week_num2;
-    $week_num4 = --$week_num3;
+    $week_num2 = $week_num1-1;
+    $week_num3 = $week_num2-1;
+    $week_num4 = $week_num3-1;
 
     $res = $connect->query("SELECT user.id,a.group_id,user.user_name,user.qq,a.status AS week1,b.status AS week2,c.status AS week3,d.status AS week4
              FROM 
-             (((report AS a INNER JOIN user ON a.user_id = user.id AND a.week_num = $week_num4) 
-             INNER JOIN report AS b ON a.user_id = b.user_id AND b.week_num = $week_num3)
-             INNER JOIN report AS c ON a.user_id = c.user_id AND c.week_num = $week_num2)
-             INNER JOIN report AS d ON a.user_id = d.user_id AND d.week_num = $week_num1 
+             (((report AS a INNER JOIN user ON a.user_id = user.id AND a.week_num = $week_num1) 
+             INNER JOIN report AS b ON a.user_id = b.user_id AND b.week_num = $week_num2)
+             INNER JOIN report AS c ON a.user_id = c.user_id AND c.week_num = $week_num3)
+             INNER JOIN report AS d ON a.user_id = d.user_id AND d.week_num = $week_num4 
              WHERE a.group_id = {$_GET['group']};");
 
     while ($foo = $res->fetch_assoc()) { $data['data'][] = $foo; }
