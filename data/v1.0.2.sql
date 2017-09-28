82e6876128668d9609f7b7f44fb7e8101cb62b9b
Gtacer <459472218@qq.com>
2017-09-28 11:14:55 +0800

    增加数据库升级语句

--- data/v1.0.2.sql
+++ data/v1.0.2.sql
@@ -2,3 +2,4 @@ ALTER TABLE user_group ADD id int PRIMARY KEY AUTO_INCREMENT FIRST;
 
 ALTER TABLE user_group ADD headsman varchar(255) AFTER deleteFlg;
 
+ALTER TABLE user MODIFY QQ varchar(255) NOT NULL;
