b9ed92aa5efcb75be01b2829a8b4068b71a15e77
Gtacer <459472218@qq.com>
2017-10-26 15:02:28 +0800

    优化自动加载代码

--- system/core/CallFunction.php
+++ system/core/CallFunction.php
@@ -10,51 +10,35 @@ require_once './system/core/TracerModels.php';
     }
 });
 
-$url_info = explode('?', $_SERVER['REQUEST_URI']);
-
+$url_info = parse_url($_SERVER['REQUEST_URI']);
 
 //获取URL中的信息
-$info = array_filter(explode('/', $url_info[0]));
-
-if ($info[1] =='index.php') {
-    foreach ($info as $key => $value) {
-        if ($value !== 'index.php')
-            array_splice($info, 0, 1);
-        else {
-            array_splice($info, 0, 1);
-            break;
-        }
-    }
-}
-
-//储存URL中通过get传入的参数值
-if (strpos($_SERVER['REQUEST_URI'], '?')) {
-    $get = explode('&', @$url_info[1]);
-    foreach ($get as $key => $value) {
-        $foo            = explode('=', $value);
-        $_GET[@$foo[0]] = @$foo[1];
-    }
+$info = array_filter(explode('/', $url_info['path']));
+if ($info[1] == 'index.php') {
+    array_shift($info);
 }
 
 
 //获取要调用的类,如果没有指定要调用的类，则默认为viewer类
 //如果直接访问网站域名，则调用view类的login方法
-if (sizeof($info) < 1)
+if (sizeof($info) < 1) {
     $class = 'viewer';
-else
+} else {
     $class = implode(array_slice($info, 0, 1));
+}
 
 
 //获取要调用的函数，如果没有指定要调用的方法，则默认为index方法
-if (sizeof($info) < 2)
+if (sizeof($info) < 2) {
     $function = 'index';
-else
+} else {
     $function = implode(array_slice($info, 1, 1));
+}
 
 //获取URL中的参数(待完善
 $parameter = array_slice($info, 2);
 
-if (sizeof($info)===1 and $info[1]=='admin'){
+if (sizeof($info) === 1 and $info[1] == 'admin') {
     $class = 'viewerb';
     $function = 'login';
 }
@@ -62,6 +46,6 @@ if (sizeof($info)===1 and $info[1]=='admin'){
 //调用
 $exc = new $class();
 
-/* call_user_func_array的返回值为回调函数执行的结果或者为false,此处代码是为防止用户通过url非法访问已存在的方法*/
+// call_user_func_array的返回值为回调函数执行的结果或者为false,此处代码是为防止用户通过url非法访问已存在的方法
 if (!@call_user_func_array(array($exc, $function), $parameter))
     die('非法访问，错误的URL');
